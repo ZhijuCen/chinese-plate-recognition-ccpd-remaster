fd1ea90964ec359308bf4d3dfe765b518ebc8462
ZhijuCen <samzikeoi@gmail.com>
2022-03-21 21:26:49 +0800

    implement training script

--- plate_detection/model_trainer.py
+++ plate_detection/model_trainer.py
@@ -1,13 +1,48 @@
 
 import data
-from model import KeypointRCNNContainer
+from model import KeypointRCNNContainer, optimizer_map
 
 from argparse import ArgumentParser, Namespace
 from pathlib import Path
 
 
+def get_list_of_ds(splits, mapping, char_annot_maps):
+    list_of_ds = list()
+    for split_loc, ds_idx in zip(splits, mapping):
+        img_paths, boxes, labels, keypoints, license_plate_annots = (
+            data.parse_split_file_to_arrays(
+                args.datasets[ds_idx], split_loc, remap_lp_annot=char_annot_maps)
+        )
+        ds = data.get_dataset(img_paths, labels, boxes, keypoints)
+        list_of_ds.append(ds)
+    return list_of_ds
+
+
 def main(args: Namespace):
-    return
+    char_annot_maps = data.load_char_annots(str(args.annot_file))
+
+    train_ds = get_list_of_ds(args.train_splits, args.train_mapping, char_annot_maps)
+    train_ds = data.concat_ds(*train_ds)
+
+    val_ds = get_list_of_ds(args.val_splits, args.val_mapping, char_annot_maps)
+    if val_ds:  # is not empty
+        val_ds = data.concat_ds(*val_ds)
+    else:
+        val_ds = None
+
+    train_loader = data.get_loader(train_ds, num_workers=6)
+    if val_ds is not None:
+        val_loader = data.get_loader(val_ds, shuffle=False, num_workers=6)
+    else:
+        val_loader = None
+
+    optimizer_params = {str(s.split('=')[0]): eval(s.split('=')[1])
+                        for s in args.optimizer_params}
+    model = KeypointRCNNContainer.new_model(
+        optimizer_map[args.optimizer], optimizer_params, "cuda")
+    model.train(train_loader, args.epochs, val_loader)
+    model.prune_model()
+    model.export_onnx("export.onnx")
 
 
 if __name__ == "__main__":
@@ -45,7 +80,7 @@ if __name__ == "__main__":
         help="Parameters for optimizer."
     )
     parser.add_argument(
-        "--epochs", type=int, default=5,
+        "--epochs", type=int, default=50,
         help="Train epochs, default: %(default)s"
     )
     parser.add_argument(
