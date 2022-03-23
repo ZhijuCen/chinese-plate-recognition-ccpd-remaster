06982a331622974ab8344749dd481ac4bf8c88fb
ZhijuCen <samzikeoi@gmail.com>
2022-03-23 14:38:42 +0800

    Add augmentation for validation set

--- plate_detection/data/augmentation.py
+++ plate_detection/data/augmentation.py
@@ -16,3 +16,14 @@ def default_keypoint_transform(
         A.Flip(p=p),
     ], bbox_params=bbox_params, keypoint_params=kp_params)
     return compose
+
+
+def identity_keypoint_transform(
+    bbox_format="pascal_voc", kp_format="xy"
+) -> A.Compose:
+    bbox_params = A.BboxParams(bbox_format)
+    kp_params = A.KeypointParams(kp_format, remove_invisible=False)
+    compose = A.Compose([
+        A.Resize(320, 320),
+    ], bbox_params=bbox_params, keypoint_params=kp_params)
+    return compose
